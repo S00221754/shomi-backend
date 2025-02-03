fc33a438c169f95c08b5ca6314cbf2381eca807c
S00221754 <S00221754@atu.ie>
2025-02-03 19:31:06 +0000

    updated auto database creation to only run when env is production

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,24 @@
 import http from 'http';
 import app from './app';
 import { createDatabaseIfNotExists } from './database/data-source';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const server = http.createServer(app);
+const ENV = process.env.NODE_ENV ?? "development";
 
-createDatabaseIfNotExists()
-  .then(() => {
-    app.listen(3000, () => console.log("🚀 Server running on http://localhost:3000"));
-  })
-  .catch((error) => {
-    console.error("Failed to initialize database:", error);
-    process.exit(1);
-  });
+// figure out a better way to do this without relying on environment variables
+if (ENV !== "production") {
+    createDatabaseIfNotExists()
+      .then(() => {
+        server.listen(3000, () => console.log("🚀 Server running on http://localhost:3000"));
+      })
+      .catch((error) => {
+        console.error("❌ Failed to initialize database:", error);
+        process.exit(1);
+      });
+  } else {
+    console.log("🚀 Running in production mode. Skipping database creation.");
+    server.listen(3000, () => console.log("🚀 Server running on http://localhost:3000"));
+  }
