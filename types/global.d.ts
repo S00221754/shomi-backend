854e70d7569dd2547f6ee73919f0a2bc92bdf137
S00221754 <S00221754@atu.ie>
2025-03-26 15:41:55 +0000

    for render deployment

---
+++ types/global.d.ts
@@ -0,0 +1,6 @@
+declare module 'express';
+declare module 'cors';
+declare module 'swagger-ui-express';
+declare module 'swagger-jsdoc';
+declare module 'jsonwebtoken';
+declare module 'http-errors';
