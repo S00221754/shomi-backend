94a6f09e029972f29fd9934355f94c8ca225d015
S00221754 <S00221754@atu.ie>
2025-03-26 21:21:17 +0000

    updaed code to work in local and prod

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -4,11 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const DB_USER = process.env.DB_USER!;
-const DB_PASS = process.env.DB_PASS!;
-const DB_NAME = process.env.DB_NAME!;
-const DB_HOST = process.env.DB_HOST!;
-const DB_PORT = Number(process.env.DB_PORT ?? "5432");
+const isProduction = process.env.NODE_ENV === "production";
 
 const AppDataSource = new DataSource({
   type: "postgres",
@@ -17,15 +13,12 @@ const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-    family: 4,
-  },
   synchronize: false,
   logging: true,
-  entities: [process.env.NODE_ENV === "production" ? "dist/entities/*.js" : "src/entities/*.ts"],
-  migrations: [process.env.NODE_ENV === "production" ? "dist/database/migrations/*.js" : "src/database/migrations/*.ts"],
+  ssl: isProduction ? { rejectUnauthorized: false } : undefined,
+  extra: isProduction ? { ssl: { rejectUnauthorized: false }, family: 4 } : {}, // family 4 is to help work with render ipv4 should be changed to 6 when approved from microsoft
+  entities: [isProduction ? "dist/entities/*.js" : "src/entities/*.ts"],
+  migrations: [isProduction ? "dist/database/migrations/*.js" : "src/database/migrations/*.ts"],
 });
+
 export default AppDataSource;
