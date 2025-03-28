a72e06c104b30fb5396588a4316d6c18ee9d58b7
S00221754 <S00221754@atu.ie>
2025-03-28 02:55:56 +0000

    Updated to include columns of recipes images

--- src/types/recipe.ts
+++ src/types/recipe.ts
@@ -1,18 +1,20 @@
 import { RecipeIngredient } from "./recipeIngredient";
 
 export type CreateRecipeDTO = {
-    recipe_name: string;
-    recipe_description: string;
-    recipe_instructions: string;
-    ingredients: RecipeIngredient[];
-    cooking_time: number;
-    author_id: string;
-}
+  recipe_name: string;
+  recipe_description: string;
+  recipe_instructions: string;
+  ingredients: RecipeIngredient[];
+  cooking_time: number;
+  author_id: string;
+  recipe_images?: string[];
+};
 
 export class UpdateRecipeDTO {
-    recipe_name?: string;
-    recipe_description?: string;
-    recipe_instructions?: string;
-    ingredients?: RecipeIngredient[];
-    cooking_time?: number;
+  recipe_name?: string;
+  recipe_description?: string;
+  recipe_instructions?: string;
+  ingredients?: RecipeIngredient[];
+  cooking_time?: number;
+  recipe_images?: string[];
 }
