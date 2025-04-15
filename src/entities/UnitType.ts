19bb6f8cc805d5b48c0b11f4d6042b03a979aff6
S00221754 <S00221754@atu.ie>
2025-04-15 17:12:18 +0100

    Added conversions to mass and volume unit types while removing some due to their incosistency for converting

--- src/entities/UnitType.ts
+++ src/entities/UnitType.ts
@@ -1,25 +1,36 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    CreateDateColumn,
-    UpdateDateColumn,
-    Unique,
-  } from "typeorm";
-  
-  @Entity("unit_types")
-  @Unique(["name"])
-  export class UnitType {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
-  
-    @Column()
-    name: string;
-  
-    @CreateDateColumn()
-    createdAt: Date;
-  
-    @UpdateDateColumn()
-    updatedAt: Date;
-  }
-  
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Unique,
+} from "typeorm";
+
+@Entity("unit_types")
+@Unique(["name"])
+export class UnitType {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  label?: string;
+
+  @Column()
+  type: "mass" | "volume";
+
+  @Column({ name: "base_unit" })
+  baseUnit: string;
+
+  @Column({ name: "multiplier_to_base", type: "float" })
+  multiplierToBase: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
